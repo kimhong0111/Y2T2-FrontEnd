1c54446cac16c9a3eb82ccf61185db991368bbd9
kimhong0111 <iceprostep@gmail.com>
2026-02-12 12:30:26 +0700

    feat : W4

---
+++ W4/exercise-2/src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+
+import viteLogo from '/vite.svg'
+import Header from './Header.jsx'
+import Footer from './Footer.jsx'
+import Main  from  './Body.jsx'
+
+function App() {
+  return(
+    <>
+    <Header></Header>
+    <Main></Main>
+    <Footer></Footer>
+    </>
+    
+  );
+    
+    
+  
+  
+}
+
+export default App
---
+++ W4/exercise-2/src/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer(){
+    return(
+        <footer>
+            <p className="block">You can find React Doc at <a href="https://react.dev/">https://react.dev</a></p>
+        </footer>
+    )
+}
+
+export default Footer;
